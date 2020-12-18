d90df2db1c6b8716b3b643e6387f59507a02e704
Ritik Sinha <ritiksinha91@gmail.com>
2020-12-18 18:07:35 +0530

    Added Dispatching Action and transformed Reducer

--- redux-basics.js
+++ redux-basics.js
@@ -16,12 +16,31 @@ const initialState = {
 
 
  //Reducer
- const rootReducer = ( currentState = initialState, action ) => currentState 
- /** The reducer will always receive an Action and the old state. For now, it is simply returning the same state. 
-  * To initialise the state for the time when the store is created, and Reducer executes for the 1st time, we can set an
-  * initial state.  
- */
+ const rootReducer = ( currentState = initialState, action ) => {
+
+    if ( action.type === 'INC_COUNTER' ) {
+        return {
+            ...currentState,
+            counter: currentState.counter + 1
+        }
+    }
+
+    if ( action.type === 'ADD_COUNTER' ) {
+        return {
+            ...currentState,
+            counter: currentState.counter + action.value
+        }
+    }
+    return currentState;
+ } 
+ 
 
  //Store
  const store = createStore(rootReducer);
+console.log(store.getState());
+
+
+//Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
