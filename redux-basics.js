66423ab046d7a30a3a8377850b2c80448fb76da4
Ritik Sinha <ritiksinha91@gmail.com>
2020-12-18 18:23:19 +0530

    Added Subscription

--- redux-basics.js
+++ redux-basics.js
@@ -40,7 +40,20 @@ const initialState = {
 console.log(store.getState());
 
 
+//Subscription 
+/** Subscription makes sure that we won't  have to call 'store.getState()' manually, to get the current state but to inform
+ * me that i need to get the current state coz something changed. The 'subscribe' method takes a function as argument,
+ * which executes on every state change. The subscribe is setup before the dispatch, so that we get informed about any
+ * future dispatches. The function inside the 'subscribe' will execute everytime, whenever an 'Action' dispatches and 
+ * mutate the store.    
+*/
+store.subscribe(() => {
+
+    console.log('Subscription', store.getState())
+})
+
 //Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
+
